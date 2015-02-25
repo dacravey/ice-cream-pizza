70ebd9f690e0d684cda2c566c7b9048f9fa14eb1
Debbie <cinemaestro@yahoo.com>
2015-02-24 18:32:50 -0800

    refactored app to coincide with Code School gem store lesson, part 1

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-var iceCreamPizza = angular.module('iceCreamPizza', []);
+(function() {
+  var app = angular.module('iceCreamPizza', []);
+
+  app.controller('RestaurantController', function(){
+    this.products = pizzas;
+    });
+
+  var pizzas = [
+  { name: 'Veggie Delight', price: 1.95, description: 'all veggies, all the time...', canPurchase: true, soldOut: false },
+  { name: 'Tofurky Jerky', price: 2.95, description: 'a bit on the chewy side', canPurchase: true, soldOut: false },
+  { name: 'Rita\'s Margarita', price: 5.95, description: 'No, we didn\'t spell that wrong - it has tequilla in the crust', canPurchase: true, soldOut: false },
+  { name: 'Cali Cauli', price: 0.95, description: 'ew gross', canPurchase: true, soldOut: false }
+  ];
+
+})();
+
+
+// var iceCream = {name: 'Rocky Road', price: 2.50, description: 'a few rocks, a peppble or two"'}
---
+++ controllers/RestaurantController.js
@@ -0,0 +1,9 @@
+app.controller('RestaurantController', function(){
+  this.product = pizza;
+  });
+
+  var pizza = {
+    name: 'Veggie Delight',
+    price: 1.95,
+    description: '. . .',
+  };
