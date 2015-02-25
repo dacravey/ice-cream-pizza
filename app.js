5a7cf934b01d60f3f54629f28225765ced8429be
Debbie <cinemaestro@yahoo.com>
2015-02-24 19:39:46 -0800

    add images for each pizza item

--- app.js
+++ app.js
@@ -6,10 +6,19 @@
     });
 
   var pizzas = [
-  { name: 'Veggie Delight', price: 1.95, description: 'all veggies, all the time...', canPurchase: true, soldOut: false },
-  { name: 'Tofurky Jerky', price: 2.95, description: 'a bit on the chewy side', canPurchase: true, soldOut: false },
-  { name: 'Rita\'s Margarita', price: 5.95, description: 'No, we didn\'t spell that wrong - it has tequilla in the crust', canPurchase: true, soldOut: false },
-  { name: 'Cali Cauli', price: 0.95, description: 'ew gross', canPurchase: true, soldOut: false }
+  { name: 'Veggie Delight', price: 1.95, description: 'all veggies, all the time...', canPurchase: true, soldOut: false, images: [
+        "img/veggie-pizza.jpg"
+      ], },
+  { name: 'Tofurky Jerky', price: 2.95, description: 'a bit on the chewy side', canPurchase: true, soldOut: false,
+  images: [
+          "img/tofurky.jpg",
+        ],  },
+  { name: 'Rita\'s Margarita', price: 5.95, description: 'No, we didn\'t spell that wrong - it has tequilla in the crust', canPurchase: true, soldOut: false, images: [
+        "img/margarita.jpg",
+      ], },
+  { name: 'Cali Cauli', price: 0.95, description: 'ew gross', canPurchase: false, soldOut: false, images: [
+        "img/cauli.jpg",
+      ], }
   ];
 
 })();
